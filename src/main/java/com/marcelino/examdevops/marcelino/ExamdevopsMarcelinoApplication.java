1cdbbf94872ea702393f21cd021cf47438b3f0be
Echebue08 <104105511+Echebue08@users.noreply.github.com>
2024-03-21 14:39:11 +0000

    Premier commit

---
+++ src/main/java/com/marcelino/examdevops/marcelino/ExamdevopsMarcelinoApplication.java
@@ -0,0 +1,13 @@
+package com.marcelino.examdevops.marcelino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamdevopsMarcelinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamdevopsMarcelinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelino/examdevops/marcelino/ExamdevopsMarcelinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelino.examdevops.marcelino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamdevopsMarcelinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
